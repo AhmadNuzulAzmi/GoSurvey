71159a441e24f0baaadee5e84a1ebb8564567e0a
AhmadNuzulAzmi <52515316+AhmadNuzulAzmi@users.noreply.github.com>
2021-07-08 22:20:40 +0700

    go

--- application/views/Member/Survey/TaskSurvey.php
+++ application/views/Member/Survey/TaskSurvey.php
@@ -83,10 +83,8 @@
                   <input type="file" name="image" class="custom-file-input" id="upload_filee">
                   <label class="custom-file-label" for="customFile">Upload Bukti Pembayaran</label>
                 </div>
-                <label class="mt-2" style="color: red;">Note : <br /><br />- Tidak perlu upload bukti pembayaran jika menggunakan metode pembayaran <b> Saldo Dompet</b></label>
-                <br />
-                <label class="mt-2,5" style="color: red;">
-                  - Bukti Pembayaran yang diupload <b>Wajib</b> dengan format: <b> jpg, dan png </b>
+                <label class="mt-2" style="color: red;">Note : <br /><br />Tidak perlu upload bukti pembayaran jika menggunakan metode pembayaran <b> Saldo Dompet</b></label>
+
 
               </div>
 
